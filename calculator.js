5f2b04fb0f05d7e87b90e16c68e8bf25d906cd98
alfonsoc2 <alfonsocenteno2@gmail.com>
2024-11-08 13:41:18 -0700

    modified js file to show current number

--- calculator.js
+++ calculator.js
@@ -1,12 +1,14 @@
 const display = document.querySelector('.display');
-
+let currentInput = '';
 
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => { 
     button.addEventListener('click', () => {
         const buttonText = button.textContent;
-        
-        display.textContent = buttonText;
+        if (buttonText >='0' && buttonText <= '9' || buttonText === ['+', '-', '*', '/'].includes(buttonText)) {
+            currentInput += buttonText;
+            display.textContent = currentInput
+        }
     });
 });
